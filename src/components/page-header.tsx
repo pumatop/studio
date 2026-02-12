e69d0a0222f78b40b66a3a7b8f32da6af17d86eb
#mo5 _1only <ssukr2@gmail.com>
2026-02-12 10:28:08 +0000

    استخدم تصميم قالب AdminLTE

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -23,7 +23,7 @@ export function PageHeader({ title }: { title: string }) {
   ) as ImagePlaceholder;
 
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 py-2">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-card px-4 sm:px-6">
       <SidebarTrigger className="sm:hidden" />
       <h1 className="text-xl font-semibold">{title}</h1>
       <div className="flex-1" />
