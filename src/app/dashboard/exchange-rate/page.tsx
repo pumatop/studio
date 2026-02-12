f35d34573dff2f200b5f658e12e2ea7ec16810d9
#mo5 _1only <ssukr2@gmail.com>
2026-02-12 11:33:36 +0000

    رتب صفحة سعر الصرف  اجعل التحكم في الصرف علي اليمين وزود عرض البطاقة الي

--- src/app/dashboard/exchange-rate/page.tsx
+++ src/app/dashboard/exchange-rate/page.tsx
@@ -4,13 +4,13 @@ import { ChangeLogCard } from "./change-log-card";
 
 export default function ExchangeRatePage() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-start">
-      <div className="lg:col-span-2 space-y-6">
-        <ExchangeRateChartCard />
-        <ChangeLogCard />
-      </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-start">
       <div className="lg:col-span-1">
         <ExchangeControlCard />
+      </div>
+      <div className="lg:col-span-1 space-y-6">
+        <ExchangeRateChartCard />
+        <ChangeLogCard />
       </div>
     </div>
   );
