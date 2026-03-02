c23223a182a5dc6abf7e7136fe8b53d99250d0bd
#mo5 _1only <ssukr2@gmail.com>
2026-03-02 01:47:50 +0000

    ولكن تم تغيير الخط  اريد استخدام خط rubic

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
         body: ['Rubik', 'sans-serif'],
         headline: ['Rubik', 'sans-serif'],
         code: ['monospace'],
